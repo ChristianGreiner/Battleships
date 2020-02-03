9c91bad16282bb6f9ac7f6427b8af6de2da019d2
Remoo-t <joseph.rieger@yahoo.de>
2020-02-03 01:47:47 +0100

    javaDoc for all classes in game

--- src/game/MultiplayerAiSavegame.java
+++ src/game/MultiplayerAiSavegame.java
@@ -1,7 +1,16 @@
 package game;
 
+/**
+ * Savegame specifically for AI games.
+ */
 public class MultiplayerAiSavegame extends Savegame {
 
+    /**
+     * Constructor for AI game saves
+     * @param id The ID of the game.
+     * @param playerMap The map of the player.
+     * @param enemyMap The map of the opponent.
+     */
     public MultiplayerAiSavegame(String id, Map playerMap, Map enemyMap) {
         super(id, playerMap, enemyMap);
         this.savegameType = SavegameType.MultiplayerAi;
--- src/game/MultiplayerSavegame.java
+++ src/game/MultiplayerSavegame.java
@@ -1,7 +1,16 @@
 package game;
 
+/**
+ * Savegame for online games
+ */
 public class MultiplayerSavegame extends Savegame {
 
+    /**
+     * Constructor for online game saves
+     * @param id The ID of the game.
+     * @param playerMap The player map.
+     * @param enemyMap The map of the opponent.
+     */
     public MultiplayerSavegame(String id, Map playerMap, Map enemyMap) {
         super(id, playerMap, enemyMap);
         this.savegameType = SavegameType.Multiplayer;
