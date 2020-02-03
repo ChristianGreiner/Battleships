acf9c5eccc4ff60814bb0101f4368f3175da4187
Remoo-t <joseph.rieger@yahoo.de>
2020-02-03 03:23:38 +0100

    javaDoc for Main menu

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import core.Game;
 
 import java.awt.*;
 
+/**
+ * The main class.
+ */
 public class Main {
 
+    /**
+     * The main method to start the game.
+     * @param args
+     */
     public static void main(String[] args) {
 
         Game game = new Game("Battleships", new Point(1280, 720));
